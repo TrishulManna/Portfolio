aeb0d1598a0cf2af6a9000633000a20d4a4b05d5
TrishulManna <trishul_hotmail.com>
2021-11-17 23:03:01 +0100

    first change

--- WebApp/nuxt.config.js
+++ WebApp/nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -37,5 +38,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    },
   }
 }
--- WebApp/pages/index.vue
+++ WebApp/pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <Tutorial/>
+  <div class="page">
+    <Header>
+
+    </Header>
+    <MainContent>
+
+    </MainContent>
+    <Footer>
+
+    </Footer>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  components: true
+}
 </script>
+
+<style lang="scss" scoped>
+.page{
+  display: grid;
+}
+</style>
